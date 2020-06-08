6717f850b01030a69b976453be2b47a668827c29
Gabriel Martins Silva <58668611+SilvaGabrielMartins@users.noreply.github.com>
2020-06-08 17:01:11 -0300

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+class A {
+    private:
+    	int A1;
+    	float A2;
+    public:
+		// Get e Set A1
+		int getA1(){return A1;}
+		void setA1(int novoValor){
+			A1 = novoValor;
+		}
+
+		// Get e Set A2
+		int getA2(){return A2;}
+		void setA2(float novoValor){
+			A2 = novoValor;
+		}
+
+		void MA1(){
+			std::cout << "MA1" << std::endl;
+		}
+
+		void MA2(){
+			std::cout << "MA2" << std::endl;
+		}
+};
