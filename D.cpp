10fdebd18edc1206ae32f6d19329404b3aaef790
Gabriel Martins <gabriel.silva12@estudante.ufla.br>
2020-06-09 00:12:31 -0300

    Criação da Classe D

---
+++ D.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+class D {
+	private:
+		string D1;
+		int D2;
+	public:
+		void MD1(){
+			std::cout << "Nome D1:" << D1 << std::endl;
+		}
+		void MD2(){
+			std::cout << "Valor D2:" << D2 << std::endl;
+		}
+};
